b2de177146d1961f5e13b06c919cf8ba0682c110
Mohammad Parsa Elahimanesh <m.p.elahimanesh@gmail.com>
2024-11-27 22:28:31 +0330

    New shuffles added

--- gen/gen.cpp
+++ gen/gen.cpp
@@ -3,21 +3,40 @@
 
 using namespace std;
 
+constexpr int TOTAL = 100;
 inline bool C() {return rnd.next(2);}
 inline bool Coin() {return C();}
 inline bool coin() {return Coin();}
-constexpr int TOTAL = 100;
 inline bool C(int x) {return rnd.next(TOTAL) < x;}
 inline bool Coin(int x) {return C(x);}
 inline bool coin(int x) {return Coin(x);}
+
 inline string RBigNum(int digits){return rnd.next("[1-9][0-9]{"+to_string(digits)+"}");}
 inline string Rword(int length){return rnd.next("[a-z]{"+to_string(length)+"}");}
 inline string RWord(int length){return rnd.next("[a-zA-Z]{"+to_string(length)+"}");}
 
+template<typename _RandomAccessIterator>
+inline void RateShuffle(_RandomAccessIterator __first, _RandomAccessIterator __last, int percent = TOTAL) {
+    while (__first != __last) {
+        if(coin(percent))
+            swap(*__first, *(__first+rnd.next(__last-__first)));
+        __first++;
+    }
+}
+
+template<typename _RandomAccessIterator>
+inline void RangeShuffle(_RandomAccessIterator __first, _RandomAccessIterator __last, int percent = TOTAL) {
+    while (__first != __last) {
+        swap(*__first, *(__first+(1LL*rnd.next(__last-__first)*percent+TOTAL-1)/TOTAL));
+        __first++;
+    }
+}
+
+
 int main(int argc, char *argv[]) 
 {
-	registerGen(argc, argv, 1);
+    registerGen(argc, argv, 1);
 
 
-	return 0;
+    return 0;
 }
--- validator/validator.cpp
+++ validator/validator.cpp
@@ -4,99 +4,98 @@ using namespace std;
 typedef long long ll;
 
 ll dfs(ll v, ll d, const vector<vector<ll>> &tr) {
-	ll s = 1;
-	for(auto u: tr[v])
-		if(u != d)
-			s += dfs(u, v, tr);
-	return s;
+    ll s = 1;
+    for(auto u: tr[v])
+        if(u != d)
+            s += dfs(u, v, tr);
+    return s;
 }
 
 inline vector<vector<ll>> ReadTree(int n, ll ind = 1) {
-	vector<vector<ll>> tr(n);
-	for(int i = 1; i < n; i++) {
-	    ll u = inf.readLong(ind, ind+n-1, "u")-ind;
-		inf.readSpace();
-		ll v = inf.readLong(ind, ind+n-1, "v")-ind;
-		inf.readEoln();
-		tr[u].push_back(v);
-		tr[v].push_back(u);
-	}
-	ensure(dfs(0,0,tr) == n);
-	return tr;
+    vector<vector<ll>> tr(n);
+    for(int i = 1; i < n; i++) {
+        ll u = inf.readLong(ind, ind+n-1, "u")-ind;
+        inf.readSpace();
+        ll v = inf.readLong(ind, ind+n-1, "v")-ind;
+        inf.readEoln();
+        tr[u].push_back(v);
+        tr[v].push_back(u);
+    }
+    ensure(dfs(0,0,tr) == n);
+    return tr;
 }
 
 // read n numbers in one line in range [a, b] inclusive.
 inline vector<ll> GetIntList(size_t n, ll l, ll r, string name = "arr") {
-	vector<ll> arr = inf.readLongs(n, l, r, name);
-	inf.readSpace();
-	return arr;
+    vector<ll> arr = inf.readLongs(n, l, r, name);
+    inf.readSpace();
+    return arr;
 }
 
 inline vector<ll> GetIntList(size_t n, string name = "arr") {
-	return GetIntList(n, LONG_LONG_MIN, LONG_LONG_MAX, name);
+    return GetIntList(n, LONG_LONG_MIN, LONG_LONG_MAX, name);
 }
 
 inline ll ReadInt(ll l, ll r, string name = "n") {
-	ll res = inf.readLong(l, r, name);
-	inf.readEoln();
-	return res;
+    ll res = inf.readLong(l, r, name);
+    inf.readEoln();
+    return res;
 }
 
 inline ll ReadInt(string name = "n") {
-	return ReadInt(LONG_LONG_MIN, LONG_LONG_MAX, name);
+    return ReadInt(LONG_LONG_MIN, LONG_LONG_MAX, name);
 }
 
 vector<ll> GetIntList(ll ln, ll rn, ll la, ll ra, string arrName = "arr", string lengthName = "n") {
-	return GetIntList(ReadInt(ln, rn, lengthName), la, ra, arrName);
+    return GetIntList(ReadInt(ln, rn, lengthName), la, ra, arrName);
 }
 
 inline void CheckSorted(const vector<ll> &arr) {
-	ensuref(is_sorted(arr.begin(), arr.end()), "Array must be sorted.");
+    ensuref(is_sorted(arr.begin(), arr.end()), "Array must be sorted.");
 }
 
 inline void CheckUnique(vector<ll> &arr) {
-	ensuref(unique(arr.begin(), arr.end()) == arr.end(), "Array must be unique.");
+    ensuref(unique(arr.begin(), arr.end()) == arr.end(), "Array must be unique.");
 }
 
 inline string word(string name = "s") {
-	string s = inf.readToken("[a-z]+", name);
-	inf.readEoln();
-	return s;
+    string s = inf.readToken("[a-z]+", name);
+    inf.readEoln();
+    return s;
 }
 
 inline string word(size_t l, size_t r, string name = "s") {
-	string s = word(name);
-	ensuref(l <= s.size() and s.size() <= r, "String size does not match.");
-	return s;
+    string s = word(name);
+    ensuref(l <= s.size() and s.size() <= r, "String size does not match.");
+    return s;
 }
 
 inline string word(size_t n, string name = "s") {
-	return word(1, n, name);
+    return word(1, n, name);
 }
 
 inline string Word(string name = "s") {
-	string s = inf.readToken("[A-Za-z]+", name);
-	inf.readEoln();
-	return s;
+    string s = inf.readToken("[A-Za-z]+", name);
+    inf.readEoln();
+    return s;
 }
 
 inline string Word(size_t l, size_t r, string name = "s") {
-	string s = Word(name);
-	ensuref(l <= s.size() and s.size() <= r, "String size does not match.");
-	return s;
+    string s = Word(name);
+    ensuref(l <= s.size() and s.size() <= r, "String size does not match.");
+    return s;
 }
 
 inline string Word(size_t n, string name = "s") {
-	return Word(1, n, name);
+    return Word(1, n, name);
 }
 
 
-int main(int argc, char** argv) 
-{
-	registerValidation(argc, argv);
-	
+int main(int argc, char** argv) {
+    registerValidation(argc, argv);
+    
 
 
-	inf.readEof();
-	return 0;
+    inf.readEof();
+    return 0;
 }
